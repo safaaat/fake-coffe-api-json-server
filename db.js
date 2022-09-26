aa9ee52239288076fdad670c086aac9e20f9cc60
Muhammad Safaat <paatlupi01@gmail.com>
2022-09-26 16:11:26 +0700

    add akunCategori,pelanggan. edit keranjangs, pesanans,products, db.js

--- db.js
+++ db.js
@@ -2,10 +2,14 @@ const products = require("./data/products");
 const pesanans = require("./data/pesanans");
 const keranjangs = require("./data/keranjangs");
 const categories = require("./data/categories");
+const akunCategori = require("./data/akunCategori.json");
+const pelanggan = require("./data/pelanggan.json");
 
 module.exports = {
   products,
   pesanans,
   keranjangs,
-  categories
+  categories,
+  akunCategori,
+  pelanggan
 }
